69463baff15f4f4be6ed39e637827fa394de07e1
marcin10lw <marcin10lw@wp.pl>
2024-04-01 23:48:56 +0200

    chore: add dotenv and hide example codes under feature flag

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@ import OTP from "@/app/components/OTP";
 import TestCodes from "./components/TestCodes";
 
 export default function Home() {
+  const showExampleCodes = process.env.SHOW_EXAMPLE_CODES;
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-slate-950 p-5">
       <div className="flex flex-col gap-8">
         <OTP OTPStructure={[3, "-", 3, "-", 3]} />
-        <TestCodes />
+        {showExampleCodes && <TestCodes />}
       </div>
     </main>
   );
