66529d3846cc5e370310ee8471205d75f3a6520b
marcin10lw <marcin10lw@wp.pl>
2024-04-01 23:51:39 +0200

    fix(CopyButton): correct svgs properties

--- app/components/CopyButton.tsx
+++ app/components/CopyButton.tsx
@@ -16,9 +16,9 @@ const CopyButton = ({ size = 14, copied, onClick }: CopyButtonProps) => {
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           >
             <path d="M20 6 9 17l-5-5" />
           </svg>
@@ -31,9 +31,9 @@ const CopyButton = ({ size = 14, copied, onClick }: CopyButtonProps) => {
           viewBox="0 0 24 24"
           fill="none"
           stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         >
           <rect width="14" height="14" x="8" y="8" rx="2" ry="2" />
           <path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2" />
