431423cfdf8c654850201dc95913a8f7a233486e
marcin10lw <marcin10lw@wp.pl>
2024-04-03 22:21:15 +0200

    refactor(OTP): refactor otp state structure

--- app/components/OTP.tsx
+++ app/components/OTP.tsx
@@ -2,7 +2,7 @@
 
 import { FormEvent, KeyboardEvent, useEffect, useRef, useState } from "react";
 
-const isDigitChar = (value: string) => value.match(/\d/g);
+const isDigitChar = (value: string) => value.match(/^\d+$/);
 
 interface OTPProps {
   OTPStructure: (number | string)[];
@@ -15,34 +15,50 @@ type OTPInput = {
   value: string;
 };
 
+const isOTPInputArray = (input: any): input is OTPInput[] =>
+  typeof input !== "string";
+
 const OTP = ({ OTPStructure, autoFocus = true, onSubmit }: OTPProps) => {
-  const [OTPInputs, setOTPInputs] = useState<(OTPInput | string)[]>([]);
+  const [OTPInputsStruct, setOTPInputsStruct] = useState<
+    (OTPInput[] | string)[]
+  >([]);
+
   const inputRefs = useRef<HTMLInputElement[]>([]);
 
-  const filteredOTPInputs = OTPInputs.filter(
-    (input): input is OTPInput => typeof input !== "string",
+  const filteredOTPInputs = OTPInputsStruct.filter(isOTPInputArray);
+
+  const inputsAmount = filteredOTPInputs.reduce(
+    (previousValue, currentValue) => previousValue + currentValue.length,
+    0,
   );
 
-  const inputsAmount = filteredOTPInputs.length;
-
-  const disabled = filteredOTPInputs.some((input) => input.value === "");
+  const disabled = filteredOTPInputs.some((struct) =>
+    struct.some((input) => input.value === ""),
+  );
 
   useEffect(() => {
-    const newInputs: (OTPInput | string)[] = [];
-    let newIndex = 0;
+    const newInputs: (OTPInput[] | string)[] = [];
+    let structIndex = 0;
+    let inputIndex = 0;
 
     OTPStructure.forEach((item) => {
       if (typeof item === "number") {
+        const inputsList: OTPInput[] = [];
+
         for (let i = 0; i < item; i++) {
-          newInputs.push({ index: newIndex, value: "" });
-          newIndex++;
+          inputsList.push({ index: inputIndex, value: "" });
+          inputIndex++;
         }
+
+        newInputs[structIndex] = inputsList;
       } else {
         newInputs.push(item);
       }
+
+      structIndex = newInputs.length;
     });
 
-    setOTPInputs(newInputs);
+    setOTPInputsStruct(newInputs);
   }, [OTPStructure, autoFocus]);
 
   const focusInput = (index: number, action?: "prev" | "next") => {
@@ -56,23 +72,33 @@ const OTP = ({ OTPStructure, autoFocus = true, onSubmit }: OTPProps) => {
   };
 
   const updateInputs = (value: string, index: number) => {
-    setOTPInputs((prevInputs) =>
-      prevInputs.map((input) => {
-        if (typeof input !== "string" && input.index === index) {
-          return {
-            ...input,
-            value,
-          };
-        }
-
-        return input;
-      }),
+    const newInputsStruct = [...OTPInputsStruct];
+    const otpStructIndex = newInputsStruct.findIndex(
+      (struct) =>
+        isOTPInputArray(struct) &&
+        struct.find((input) => input.index === index),
     );
+
+    const inputList = newInputsStruct[otpStructIndex];
+
+    if (isOTPInputArray(inputList)) {
+      const inputArrayIndex = inputList.findIndex(
+        (input) => input.index === index,
+      );
+
+      inputList[inputArrayIndex].value = value;
+    }
+
+    setOTPInputsStruct(newInputsStruct);
   };
 
   const onInputChange = (value: string, index: number) => {
-    if (value.length > 0 && isDigitChar(value.trim())) {
-      updateInputs(value.slice(-1), index);
+    const lastChar = value.slice(-1).trim();
+
+    if (!isDigitChar(lastChar)) return;
+
+    if (value.length > 0) {
+      updateInputs(lastChar, index);
       focusInput(index, "next");
     }
   };
@@ -103,7 +129,7 @@ const OTP = ({ OTPStructure, autoFocus = true, onSubmit }: OTPProps) => {
         sanitizedCode += char;
       }
     });
-    console.log(sanitizedCode);
+
     return sanitizedCode.slice(0, inputsAmount);
   };
 
@@ -129,68 +155,78 @@ const OTP = ({ OTPStructure, autoFocus = true, onSubmit }: OTPProps) => {
 
     if (disabled) return;
 
-    const finalCode = OTPInputs.reduce((acc, input) => {
-      if (typeof input !== "string") {
-        return acc + input.value;
+    const finalCode = OTPInputsStruct.reduce((acc, struct) => {
+      if (isOTPInputArray(struct)) {
+        return acc + struct.reduce((acc, input) => acc + input.value, "");
       }
 
-      return acc + input;
+      return acc + struct;
     }, "") as string;
 
     onSubmit(finalCode);
   };
 
-  return (
-    OTPInputs.length > 0 && (
-      <section className="rounded-md bg-slate-800 p-6">
-        <h1 className="mb-6 text-center text-2xl text-slate-200">
-          Enter verification code
-        </h1>
-        <form
-          onSubmit={onCodeVerify}
-          noValidate
-          className="flex flex-col items-center gap-6"
-        >
-          <div className="flex items-center gap-1 md:gap-2">
-            {OTPInputs.map((input, listIndex) => {
-              if (typeof input !== "string") {
+  return OTPInputsStruct.length > 0 ? (
+    <section className="rounded-md bg-slate-800 p-6">
+      <h1 className="mb-8 text-center text-2xl text-slate-200">
+        Enter verification code
+      </h1>
+      <form
+        onSubmit={onCodeVerify}
+        noValidate
+        className="flex flex-col items-center gap-10"
+      >
+        <div className="relative">
+          <div className="flex flex-col items-center gap-1 sm:flex-row md:gap-2">
+            {OTPInputsStruct.map((struct, structIndex) => {
+              if (isOTPInputArray(struct)) {
                 return (
-                  <input
-                    key={listIndex}
-                    value={input.value}
-                    onChange={({ target }) =>
-                      onInputChange(target.value, input.index)
-                    }
-                    onKeyDown={(event) => onKeyDown(event, input.index)}
-                    onPaste={onPaste}
-                    ref={(el) => {
-                      el && (inputRefs.current[input.index] = el);
-                    }}
-                    autoFocus={autoFocus && input.index === 0}
-                    className="h-16 w-full max-w-16 border border-slate-400 bg-transparent text-center text-xl text-slate-200"
-                    type="tel"
-                  />
+                  <div key={structIndex} className="flex items-center gap-2">
+                    {struct.map((input) => (
+                      <input
+                        key={input.index}
+                        value={input.value}
+                        onChange={({ target }) =>
+                          onInputChange(target.value, input.index)
+                        }
+                        onKeyDown={(event) => onKeyDown(event, input.index)}
+                        onPaste={onPaste}
+                        ref={(el) => {
+                          el && (inputRefs.current[input.index] = el);
+                        }}
+                        autoFocus={autoFocus && input.index === 0}
+                        className="h-16 w-full max-w-16 border border-slate-400 bg-transparent text-center text-xl text-slate-200"
+                        type="tel"
+                      />
+                    ))}
+                  </div>
                 );
               }
 
               return (
-                <span key={listIndex} className="text-xl text-slate-200">
-                  {input}
+                <span key={structIndex} className="text-xl text-slate-200">
+                  {struct}
                 </span>
               );
             })}
           </div>
 
-          <button
-            disabled={disabled}
-            className="w-full rounded-sm bg-blue-600 p-3 text-xl tracking-wide text-white hover:bg-blue-500 disabled:pointer-events-none disabled:opacity-70"
-          >
-            Verify
-          </button>
-        </form>
-      </section>
-    )
-  );
+          {disabled && (
+            <p className="absolute top-[calc(100%_+_6px)] w-full text-center text-rose-700">
+              Please fill all fields
+            </p>
+          )}
+        </div>
+
+        <button
+          disabled={disabled}
+          className="w-full rounded-sm bg-blue-600 p-3 text-xl tracking-wide text-white hover:bg-blue-500 disabled:pointer-events-none disabled:opacity-70"
+        >
+          Verify
+        </button>
+      </form>
+    </section>
+  ) : null;
 };
 
 export default OTP;
